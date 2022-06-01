e28b47ff2dd8fc5e0c3e5055daf9432d53a91101
OlOlsing <gomdi202@naver.com>
2022-06-01 15:33:34 +0900

    MoveTest

--- 4_Team/4_TeamClient/JunPlayer.cpp
+++ 4_Team/4_TeamClient/JunPlayer.cpp
@@ -4,33 +4,49 @@
 
 CJunPlayer::CJunPlayer()
 {
+
 }
 
 
 CJunPlayer::~CJunPlayer()
 {
+
 }
 
 void CJunPlayer::Initialize(void)
 {
+	//int iArray[4] = { 0,0,50,50 };
 	m_tInfo.vPos = { 0.f,0.f,0.f };
 	m_tInfo.vDir = { 1.f,0.f,0.f };
-
 }
 
 void CJunPlayer::Update(void)
 {
+
+	if (iArray[0] < 500)
+	{
+		for (int i = 0; i < 4; ++i)
+			iArray[i] += 3;
+	}
+
 }
 
 void CJunPlayer::Late_Update(void)
 {
+
 }
 
 void CJunPlayer::Render(HDC hDC)
 {
-	Rectangle(hDC, 50, 50, 200, 250);
+	Ellipse(hDC, iArray[0], iArray[1], iArray[2], iArray[3]);
 }
 
 void CJunPlayer::Release(void)
 {
+
+}
+
+void CJunPlayer::Key_Input(void)
+{
+
 }
--- 4_Team/4_TeamClient/JunPlayer.h
+++ 4_Team/4_TeamClient/JunPlayer.h
@@ -13,5 +13,12 @@ public:
 	virtual void Late_Update(void) override;
 	virtual void Render(HDC hDC) override;
 	virtual void Release(void) override;
+
+
+	
+private:
+	virtual void Key_Input(void) override;
+private:
+	int iArray[4] = { 0,0,50,50 };
 };
 
